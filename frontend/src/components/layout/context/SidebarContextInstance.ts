6c92a515d888cf46ca7f3d649bfd51e10f59be1e
Dgimenezdeveloper <dgimenez.developer@gmail.com>
2026-07-04 23:03:31 +0000

    ARCHIVOS HUÉRFANOS ELIMINADOS

--- frontend/src/components/layout/context/SidebarContextInstance.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-interface SidebarContextType {
-  isMobile: boolean;
-  isOpen: boolean;
-  toggleOpen: () => void;
-}
-
-export const SidebarContext = createContext<SidebarContextType | undefined>(undefined);
