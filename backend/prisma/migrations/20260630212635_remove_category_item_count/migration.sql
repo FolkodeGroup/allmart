82805798cfc3a308c881903b3b47ea8cb9944bde
Mauricio <zub_gg@hotmail.com>
2026-07-05 01:09:09 -0300

    migraciones

--- backend/prisma/migrations/20260630212635_remove_category_item_count/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `item_count` on the `categories` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "categories" DROP COLUMN "item_count";
--- backend/prisma/migrations/20260704162122_fix_schema_alignments/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX "idx_products_description_trgm";
-
--- DropIndex
-DROP INDEX "idx_products_name_trgm";
-
--- DropIndex
-DROP INDEX "idx_products_sku_trgm";
