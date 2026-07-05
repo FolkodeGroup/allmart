833e098a3f8b28d5a09164a7e90bda44b12b1ec6
Dario Sebastian Gimenez <dgimenez.developer@gmail.com>
2026-07-05 06:35:26 -0300

    Revert "1212 renderizar los gráficos como tablas legibles en el pdf de reportes"

---
+++ backend/prisma/migrations/20260630212635_remove_category_item_count/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `item_count` on the `categories` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "categories" DROP COLUMN "item_count";
